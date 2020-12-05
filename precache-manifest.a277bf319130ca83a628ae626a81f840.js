c56eedddb4fa0b95e71dec537a76a0af9335e41d
Ian Holladay <holladayian@gmail.com>
2020-12-05 10:12:32 -0900

    Updates

---
+++ precache-manifest.a277bf319130ca83a628ae626a81f840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1678a392bb5d61706527a0088ae24fe5",
+    "url": "/ih-rancidtomatillos/index.html"
+  },
+  {
+    "revision": "a8c080b171b5384f47f8",
+    "url": "/ih-rancidtomatillos/static/css/main.5b87f3dc.chunk.css"
+  },
+  {
+    "revision": "5af197259adb70db9303",
+    "url": "/ih-rancidtomatillos/static/js/2.b12c4ec2.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/ih-rancidtomatillos/static/js/2.b12c4ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c080b171b5384f47f8",
+    "url": "/ih-rancidtomatillos/static/js/main.0269227f.chunk.js"
+  },
+  {
+    "revision": "39650a546dd268510cff",
+    "url": "/ih-rancidtomatillos/static/js/runtime-main.95afb5c6.js"
+  }
+]);
